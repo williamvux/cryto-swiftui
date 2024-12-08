396d03dd839a6b075f19978aa6d67c16a8a9f11c
William Vux <chungvux163@gmail.com>
2024-12-08 09:20:22 +0700

    Initial Commit

---
+++ crypto/cryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  cryptoApp.swift
+//  crypto
+//
+//  Created by William Vux on 8/12/24.
+//
+
+import SwiftUI
+
+@main
+struct cryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
